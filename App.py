d04d92ec48982e71f8fc00476d4fe0edeeccb5e0
heejin131 <journeyjean131@gmail.com>
2025-02-04 12:09:27 +0900

    차트추가

--- App.py
+++ App.py
@@ -1,10 +1,27 @@
 import streamlit as st
+import pandas as pd 
+import matplotlib.pyplot as plt
 
 st.write('''
 # My firts APP
 Hello *HEEJIN!*
 
-![img](https://image.kmib.co.kr/online_image/2017/1128/611311110011938257_1.jpg)
+![img](https://cdn.news.hidoc.co.kr/news/photo/202104/24374_58382_0822.jpg)
 ''')
+
+df = pd.read_csv('note/lunch_menu.csv')
+
+start_index= df.columns.get_loc('2025-01-07')
+mdf = df.drop(columns=['gmail', 'github', 'domain', 'vercel', 'role'])
+df_melt = mdf.melt(id_vars=['ename'], var_name='dt', value_name='menu_name')
+
+not_na_df = df_melt[~df_melt['menu_name'].isin(['-', 'x', '<결석>'])]
+gdf=not_na_df.groupby('ename')['menu_name'].count().reset_index()
+# gdf.plot(x='ename', y='menu_name', kind='bar')
+
+# Matplotlib로 바 차트 그리기
+fig, ax = plt.subplots()
+gdf.plot(x='ename', y='menu_name', kind='bar', ax=ax)
+st.pyplot(fig)
 
 
